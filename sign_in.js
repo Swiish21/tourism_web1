83dd3a768524517bb507bba505c5d24465e37327
Malcom Otieno <malcolmotieno16@gmail.com>
2024-09-02 21:53:58 +0300

    added new js files, made new booking and signup  pages, added functionality to the website

--- sign_in.js
+++ sign_in.js
@@ -1,11 +1,148 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDWJDFwt7EMHloKVjosGqUSULI0Kh6eBXw",
+  authDomain: "visit-ugandadb.firebaseapp.com",
+  projectId: "visit-ugandadb",
+  storageBucket: "visit-ugandadb.appspot.com",
+  messagingSenderId: "182100927064",
+  appId: "1:182100927064:web:6edf3ffb4db8984bd0e02d",
+  measurementId: "G-6P2EZWZQW3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const analytics = getAnalytics(app);
+
+const main = document.getElementById("main");
+const returnBtn = document.getElementById("return-btn");
+
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const submitButton = document.getElementById("submit");
+const signupButton = document.getElementById("sign-up");
+
+const signupEmailIn = document.getElementById("email-signup");
+const confirmSignupEmailIn = document.getElementById("confirm-email-signup");
+const signupPasswordIn = document.getElementById("password-signup");
+const confirmSignUpPasswordIn = document.getElementById(
+  "confirm-password-signup"
+);
+const createacct = document.getElementById("create-acct");
+
+// Start with this
+const createacctbtn = document.getElementById("create-acct-btn");
+
+var email,
+  password,
+  signupEmail,
+  signupPassword,
+  confirmSignupEmail,
+  confirmSignUpPassword;
+
+createacctbtn.addEventListener("click", function () {
+  var isVerified = true;
+
+  signupEmail = signupEmailIn.value;
+  confirmSignupEmail = confirmSignupEmailIn.value;
+  if (signupEmail != confirmSignupEmail) {
+    window.alert("Email fields do not match. Try again.");
+    isVerified = false;
+  }
+
+  signupPassword = signupPasswordIn.value;
+  confirmSignUpPassword = confirmSignUpPasswordIn.value;
+  if (signupPassword != confirmSignUpPassword) {
+    window.alert("Password fields do not match. Try again.");
+    isVerified = false;
+  }
+
+  if (
+    signupEmail == null ||
+    confirmSignupEmail == null ||
+    signupPassword == null ||
+    confirmSignUpPassword == null
+  ) {
+    window.alert("Please fill out all required fields.");
+    isVerified = false;
+  }
+
+  if (isVerified) {
+    createUserWithEmailAndPassword(auth, signupEmail, signupPassword)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+        window.alert("Success! Account created.");
+        window.location = "./destinations/destinations.html";
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+        window.alert("Error occurred. Try again.");
+        window.alert(errorMessage);
+      });
+  }
 });
 
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
+submitButton.addEventListener("click", function () {
+  email = emailInput.value;
+  console.log(email);
+  password = passwordInput.value;
+  console.log(password);
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+
+      window.alert("Success! Welcome back!");
+      window.location = "./destinations/destinations.html";
+
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      window.alert("Error occurred. Try again.");
+    });
 });
+
+// Navigate through the login / Register components
+
+signupButton.addEventListener("click", () => {
+  main.style.display = "none";
+  createacct.style.display = "block";
+});
+
+returnBtn.addEventListener("click", function () {
+  main.style.display = "block";
+  createacct.style.display = "none";
+});
+
+analytics.logEvent("notification_received");
+
+
+
+
+// const signUpButton = document.getElementById('signUp');
+// const signInButton = document.getElementById('signIn');
+// const container = document.getElementById('container');
+
+// signUpButton.addEventListener('click', () => {
+// 	container.classList.add("right-panel-active");
+// });
+
+// signInButton.addEventListener('click', () => {
+// 	container.classList.remove("right-panel-active");
+// });
+
+
